f279250da50366fbfa1a8d746bc90426a0ef3615
Amro <amroosman04@gmail.com>
2023-09-10 01:46:59 -0700

    vc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print lowercase
+ *
+ * Return: 0 (success)
+ */
+
+{
+	int main(void)
+
+		char ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++
+	}
+	return (0);
+}
