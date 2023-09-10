e25ee3b10e198714762c3aaf18e8a3c262f504bd
Amro <amroosman04@gmail.com>
2023-09-10 03:00:45 -0700

    ggf

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabets in lowercase except q and e
+ *
+ * Return: 0 (succes)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == q || ch == e)
+			ch++;
+
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
