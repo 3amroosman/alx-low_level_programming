3c0e32938c900f049ee5a6276d0543b70bc77ec5
Amro <amroosman04@gmail.com>
2023-09-11 14:44:09 -0700

    jjjv

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: hexadecimals
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 102)
+	{
+		if (x <= 57)
+		x += 39;
+		putchar(x)
+		x++;
+	}
+	putchat('\n');
+	return (0);
+}
+
+
