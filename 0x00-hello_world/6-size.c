904666421e0bfc33c291e72ce6e4c67ee5ab74c5
Amro <amroosman04@gmail.com>
2023-09-11 19:37:33 -0700

    kjh

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print sizes
+ *
+ * Return: 0 (success0
+ */
+
+int main(void)
+{
+	printf("size of a", sizeof (char));
+	printf("size of a", sizeof (int));
+	printf("size of a", sizeof (long int));
+	printf("size of a", sizeof (long long int));
+	printf("size of a", sizeof (float));
+
+	return (0);
+}
