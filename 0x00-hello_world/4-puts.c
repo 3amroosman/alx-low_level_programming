3aa2ae874e2a693c8a497dc33f19e89f0a67c100
Amro <amroosman04@gmail.com>
2023-09-11 19:13:45 -0700

    mjh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description
+ *
+ * Return: 0 (success)
+ */
+
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   returt (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
