fc712e281310248f3ef00c01cf25c3ce636a8e63
Amro <amroosman04@gmail.com>
2023-09-14 18:32:10 -0700

    lkj

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print - numbers except 2 and 4
+ *
+ * REturn: always 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	for (num = 0; num <= 9; num++)
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
