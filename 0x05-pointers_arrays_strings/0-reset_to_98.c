bb342ec43bd9db46dc5d41c330970bfb18a8d317
Amro <amroosman04@gmail.com>
2023-09-19 15:15:52 -0700

    khj

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -16,4 +16,4 @@ void reset_to_98(int *n)
 
 {
 	*n = 98;
-}	
+}
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -5,11 +5,12 @@
  *
  * @c: the character to print
  *
- * return: 0 in success 
+ * return: 0 in success
  *
  * on error -1 is returned
  */
 
 int _putchar(char c)
-
-	return (write(1, &c, 1));	
+{
+	return (write(1, &c, 1));
+}
