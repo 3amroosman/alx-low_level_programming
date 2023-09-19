8a55d9e908e0dc671227791b7ddd424e97ed609d
Amro <amroosman04@gmail.com>
2023-09-18 18:15:13 -0700

    ldds

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - take the pointer to an int parameter and change value to 98
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+ */
 
 
 
 
 
 void reset_to_98(int *n)
+
 {
 	*n = 98;
 }	
